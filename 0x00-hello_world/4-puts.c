5180334cb45cbc9df684fc4883e5450d5dbc891f
Ahmedelshafei2361 <Ahmedelshafei2361@gmail.com>
2023-06-15 17:39:57 -0700

    betty style  Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-int main (void)
+int main(void)
 {
-	char name[]="\"Programming is like building a multilingual puzzle";
-	puts(name);
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
