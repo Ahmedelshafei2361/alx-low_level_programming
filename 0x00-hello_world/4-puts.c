dfa38337d072a91c648df09ad7aefb4a9b5efbbd
Ahmedelshafei2361 <Ahmedelshafei2361@gmail.com>
2023-06-15 18:14:36 -0700

    4-puts betty style 0.3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-	  /**
-	  * main - Entry point
-	  *  *
-	  *   * Return: Always 0
-	  *    */
+	/**
+	 * trial
+	 *
+	 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
