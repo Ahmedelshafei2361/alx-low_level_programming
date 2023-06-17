057ca50b2bad07a5396d05197411aa062b7db5ef
Ahmedelshafei2361 <Ahmedelshafei2361@gmail.com>
2023-06-16 17:56:14 -0700

    insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char as;
+	char sa;
+
+	for (as = 'a'; as <= 'z'; as++)
+	{
+		putchar(as);
+	}
+	putchar('\n');
+
+	return (0);
+}
