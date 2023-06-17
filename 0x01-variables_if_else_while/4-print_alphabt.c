982dae76c09b1c8e533349ac60b8c524ed51ddcc
Ahmedelshafei2361 <Ahmedelshafei2361@gmail.com>
2023-06-16 18:40:21 -0700

    4, alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char as;
+
+	for (as = 'a'; as <= 'z'; as++)
+	{
+		if (as == q || as == e)
+		{
+			continue;
+		putchar(as);
+		}
+	}
+		return (0);
+}
