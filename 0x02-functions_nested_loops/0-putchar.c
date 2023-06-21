f45e92097e8b0fe46f9c42c3dc2a7c2784c31063
Ahmedelshafei2361 <Ahmedelshafei2361@gmail.com>
2023-06-20 18:29:09 -0700

    0, putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,5 +8,14 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 }
