34db0f7bb37086df83394d4b7821849c349c1cf9
Ahmedelshafei2361 <Ahmedelshafei2361@gmail.com>
2023-06-21 18:57:14 -0700

    agent Jack Bauer, 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,31 +2,39 @@
 #include <stdio.h>
 
 /**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Description: This function uses two nested loops to iterate over
+ * the hours and minutes of the day. The _putchar function is used
+ * to print each character of the output.
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+	for (m = 0; m < 60; m++)
+	{
+		_putchar(h / 10 + '0');
+		_putchar(h % 10 + '0');
+		_putchar(':');
+		_putchar(m / 10 + '0');
+		_putchar(m % 10 + '0');
+		_putchar('\n');
+		}
+	}
+}
+/**
  * main - prints every minute of the day in
  * 00:00 formula
  *
  * Return: 0
  */
-
 int main(void)
 {
-	void jack_bauer(void)
-	{
-		int h;
-		int m;
-
-		for (h = 0; h <= 24; h++)
-		{
-			for (m = 0; m <= 60; m++)
-			{
-				_putchar(h / 10 + '0');
-				_putchar(h % 10 + '0');
-				_putchar(':');
-				_putchar(m / 10 + '0');
-				_putchar(m % 10 + '0');
-				_putchar('\n');
-			}
-		}
-	}
 	jack_bauer();
+
+	return (0);
 }
